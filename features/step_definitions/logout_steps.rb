631732b6825f7aef486176ebd49daf4d2db179fc
Jonathan Arnett <JArnett@covermymeds.com>
2015-05-27 13:08:27 -0400

    Logout feature and steps

--- features/step_definitions/logout_steps.rb
+++ features/step_definitions/logout_steps.rb
@@ -1,1 +1,15 @@
-#Add your codez here
+Given(/^I am on the secret page$/) do
+  visit('/dashboard')
+end
+
+When(/^I press the logout button$/) do
+  click_on("Logout")
+end
+
+Then(/^I should be on the login page$/) do
+  expect(page).to have_content("This is login page for users.")
+end
+
+Then(/^I should see the logout text$/) do
+  expect(page).to have_content("You have been logged out")
+end
