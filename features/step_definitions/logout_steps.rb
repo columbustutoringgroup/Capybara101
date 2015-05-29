b68e79f979e549cec7e09f5bedd90d94a6d5b97d
LoRyder1 <lpnag@CMDVMOSXLT075.local>
2015-05-29 10:58:30 -0400

    wrote feature test

--- features/step_definitions/logout_steps.rb
+++ features/step_definitions/logout_steps.rb
@@ -1,1 +1,16 @@
-#Add your codez here
+### private/secret page is not properly hidden you don't need to log in to reach it
+
+Given(/^the private page$/) do
+	visit '/'
+	fill_in 'username', with: 'wow'
+	fill_in 'password', with: 'wow'
+	click_button 'Login'
+end
+
+When(/^I click logout link$/) do
+	click_link('Logout')
+end
+
+Then(/^I should go back to home page$/) do
+	expect(page).to have_content 'This is login page for users'
+end
