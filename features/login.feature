0d0930c1bdb1cf3e941ee8c551adc57fb26f71e4
Gabriel Escamilla <GEscamilla@covermymeds.com>
2015-10-07 11:42:03 -0400

    added test for wrong user used the same URL to corfirm the landing page and a selector for content

--- features/login.feature
+++ features/login.feature
@@ -7,4 +7,8 @@ Feature: Login Page
     Given the login page
     When I log in with proper credentials
     Then I should see the secret page
-    
+
+  Scenario: Invalid User
+    Given the login page
+    When I log in with invalid credentials
+    Then I should see the login page with an error message
--- features/step_definitions/login_steps.rb
+++ features/step_definitions/login_steps.rb
@@ -12,3 +12,16 @@ Then(/^I should see the secret page$/) do
   url = URI.parse(current_url)
   expect(url.path).to eq('/dashboard')
 end
+
+
+When(/^I log in with invalid credentials$/) do
+  fill_in('username', :with => "Admin")
+  fill_in('password', :with => "Wrong Password")
+  click_button('Login')
+end
+
+Then(/^I should see the login page with an error message$/) do
+  url = URI.parse(current_url)
+  expect(url.path).to eq('/login')
+  expect(page.has_content?("Your username & password did not match")).to be true
+end
