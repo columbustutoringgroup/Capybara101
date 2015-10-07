89fec7538e9d1733fd1a0ad85a0f8612b157c27d
Gabriel Escamilla <GEscamilla@covermymeds.com>
2015-10-07 11:35:11 -0400

    first test passed. I did not get the current_path from the session so I used the url

--- features/step_definitions/login_steps.rb
+++ features/step_definitions/login_steps.rb
@@ -1,14 +1,14 @@
 Given(/^the login page$/) do
-  #put your code here
-  pending
+  visit('/')
 end
 
 When(/^I log in with proper credentials$/) do
-  #put your code here
-  pending
+  fill_in('username', :with => "Admin")
+  fill_in('password', :with => "Admin")
+  click_button('Login')
 end
 
 Then(/^I should see the secret page$/) do
-  #put your code here
-  pending
+  url = URI.parse(current_url)
+  expect(url.path).to eq('/dashboard')
 end
