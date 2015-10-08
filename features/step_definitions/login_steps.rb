dfa734fb9435b07e6959186b1be39e5d1f7b9866
Gabriel Escamilla <GEscamilla@covermymeds.com>
2015-10-08 10:12:14 -0400

    expect without '.to' ran but is wrong so here is the fix. Also udated some ruby 2 syntax support

--- features/step_definitions/login_steps.rb
+++ features/step_definitions/login_steps.rb
@@ -3,21 +3,21 @@ Given(/^the login page$/) do
 end
 
 When(/^I log in with proper credentials$/) do
-  fill_in('username', :with => "Admin")
-  fill_in('password', :with => "Admin")
+  fill_in('username', with: "Admin")
+  fill_in('password', with: "Admin")
   click_button('Login')
 end
 
 Then(/^I should see the secret page$/) do
   url = URI.parse(current_url)
   expect(url.path).to eq('/dashboard')
-  expect(page.has_content?("Your username is Admin"))
+  expect(page.has_content?("Your username is Admin")).to be true
 end
 
 
 When(/^I log in with invalid credentials$/) do
-  fill_in('username', :with => "Admin")
-  fill_in('password', :with => "Wrong Password")
+  fill_in('username', with: "Admin")
+  fill_in('password', with: "Wrong Password")
   click_button('Login')
 end
 
