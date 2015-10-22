fdb2157b654d19228a451b98ae45320bcf7e6237
Nick Legg <nlegg@covermymeds.com>
2015-10-22 09:34:40 -0400

    Clean up quotes and other syntax

--- features/step_definitions/login_steps.rb
+++ features/step_definitions/login_steps.rb
@@ -1,25 +1,25 @@
 Given(/^the login page$/) do
-  visit("/")
+  visit('/')
 end
 
 When(/^I log in with proper credentials$/) do
-  fill_in("username", :with => "User")
-  fill_in("password", :with => "User")
-  click_button("Login")
+  fill_in('username', with: 'User')
+  fill_in('password', with: 'User')
+  click_button('Login')
 end
 
 Then(/^I should see the secret page$/) do
-  expect(current_path).to eq "/dashboard"
-  expect(page).to have_content("Your username is User")
+  expect(current_path).to eq '/dashboard'
+  expect(page).to have_content('Your username is User')
 end
 
 When(/^I log in with improper credentials$/) do
-  fill_in("username", :with => "User")
-  fill_in("password", :with => "Wrong Password")
-  click_button("Login")
+  fill_in('username', with: 'User')
+  fill_in('password', with: 'Wrong Password')
+  click_button('Login')
 end
 
 Then(/^I should see the login page with error message$/) do
-  expect(current_path).to eq "/login"
-  expect(page).to have_content("Your username & password did not match")
+  expect(current_path).to eq '/login'
+  expect(page).to have_content('Your username & password did not match')
 end
--- features/step_definitions/logout_steps.rb
+++ features/step_definitions/logout_steps.rb
@@ -1,20 +1,20 @@
 Given(/^the dashboard page$/) do
-  step "the login page"
-  step "I log in with proper credentials"
+  step 'the login page'
+  step 'I log in with proper credentials'
 end
 
 When(/^I click the Logout link$/) do
-  click_on("Logout")
+  click_on('Logout')
 end
 
 Then(/^I should see the home page$/) do
-  expect(current_path).to eq "/logout"
+  expect(current_path).to eq '/logout'
 end
 
 When(/^I visit the dashboard$/) do
-  visit("/dashboard")
+  visit('/dashboard')
 end
 
 Then(/^I should not see my username$/) do
-  expect(page).to have_no_content("Your username is User.")
+  expect(page).to have_no_content('Your username is User.')
 end
