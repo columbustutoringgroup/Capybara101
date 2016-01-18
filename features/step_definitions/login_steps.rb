2d9d8bcb33a1fbbeafa1bae168de6949d44554f8
Rajashree K Maiya <rajashree.maiya@gmail.com>
2016-01-18 14:10:46 -0500

    rmaiya-solution

--- features/step_definitions/login_steps.rb
+++ features/step_definitions/login_steps.rb
@@ -1,16 +1,13 @@
 Given(/^the login page$/) do
-  #put your code here
   visit '/'
 end
 
 When(/^I log in with proper credentials$/) do
-  #put your code here
   fill_in 'username', with: 'rmaiya'
   fill_in 'password', with: 'rmaiya'
   click_button 'Login'
 end
 
 Then(/^I should see the secret page$/) do
-  #put your code here
   expect(page).to have_content 'This is the secret page.'
 end
