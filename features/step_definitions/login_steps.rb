fe176a48ef5276afdaa1ea28edd9b061201f5115
Christian Choi <cchoi@CMDVMOSXLT143.local>
2016-01-18 13:37:50 -0500

    Completed features and tests.

--- features/step_definitions/login_steps.rb
+++ features/step_definitions/login_steps.rb
@@ -1,14 +1,21 @@
 Given(/^the login page$/) do
-  #put your code here
-  pending
+  visit '/'
 end
 
 When(/^I log in with proper credentials$/) do
-  #put your code here
-  pending
+  within("#session") do
+    fill_in 'Username', :with => 'cchoi'
+    fill_in 'Password', :with => 'cchoi'
+  end
+  click_button 'Login'
 end
 
 Then(/^I should see the secret page$/) do
-  #put your code here
-  pending
+  expect(current_path).to eq '/dashboard'
+end
+
+And(/^I should see the content "(.*?)"$/) do |text|
+  within("#session") do
+    page.should have_content text
+  end
 end
