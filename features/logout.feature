70fb3b5246251baacff682afeb75dbf591b170ef
marcel <mkooi@covermymeds.com>
2016-01-20 09:54:12 -0500

    marcel-soln

--- features/logout.feature
+++ features/logout.feature
@@ -1,1 +1,11 @@
-#Add your codez here
+Feature: Logout Page
+  In order to keep out the good guys good
+  As a user
+  I want to have to logout when I click logout
+
+  Scenario: Authenticated User
+    Given I am logged in
+    When I visit the secret page
+    And I log out
+    Then I should see the home page with a logged out notification
+    
--- features/step_definitions/logout_steps.rb
+++ features/step_definitions/logout_steps.rb
@@ -1,1 +1,20 @@
-#Add your codez here
+Given(/^I am logged in$/) do
+  visit '/'
+  fill_in 'username', with: 'proper_cred'
+  fill_in 'password', with: 'proper_cred'
+  click_button 'Login'
+end
+
+When(/^I visit the secret page$/) do
+  visit '/dashboard'
+end
+
+When(/^I log out$/) do
+  click_link('Logout')
+end
+
+Then(/^I should see the home page with a logged out notification$/) do
+  expect(page.current_path).to eq '/logout'
+  page.should have_content('This is login page for users.')
+  page.should have_content('You have been logged out')
+end
