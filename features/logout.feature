82438402bdeaf182ba861df291d27fc54a0f0961
Josh Graber <jgraber@covermymeds.com>
2016-06-13 14:31:42 -0400

    Implement logout feature
    Added gherkin for logout proving that when user logs out they are redirected to the index and can't go to the dashboard any more.

--- features/logout.feature
+++ features/logout.feature
@@ -1,1 +1,12 @@
-#Add your codez here
+Feature: Logout Page
+  In order to lock the page when we are finished
+  As a user
+  I want get redirected back to the login page after logging out
+    and not be able to return to the secret page
+
+  Scenario: Logout User
+    Given I am logged in
+    And I am on my dashboard
+    When I click log out
+    Then I should see the index page
+    And I should not have access to the secret page
--- features/step_definitions/logout_steps.rb
+++ features/step_definitions/logout_steps.rb
@@ -1,1 +1,26 @@
-#Add your codez here
+Given /^I am logged in$/ do
+  visit '/'
+  fill_in 'username', :with => 'Me'
+  fill_in 'password', :with => 'Me'
+  click_button 'Login'
+end
+
+Given /^I am on my dashboard$/ do
+  visit '/dashboard'
+end
+
+When /^I click log out$/ do
+  visit '/logout'
+end
+
+Then /^I should see the index page$/ do
+  expect(page).to have_content 'This is login page for users.'
+end
+
+Then /^I should not have access to the secret page$/ do
+  visit '/dashboard'
+
+  expect(page).to have_content 'This is login page for users.'
+  expect(current_path).to eq '/'
+end
+
