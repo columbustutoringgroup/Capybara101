b71adbd5c62d56fc1fb204344423c6ee5e6bf15e
Josh Meek <jmeek@covermymeds.com>
2016-06-13 13:09:23 -0400

    finish login feature

--- features/step_definitions/login_steps.rb
+++ features/step_definitions/login_steps.rb
@@ -1,14 +1,14 @@
 Given(/^the login page$/) do
-  #put your code here
-  pending
+  visit '/'
 end
 
-When(/^I log in with proper credentials$/) do
-  #put your code here
-  pending
+When(/^I log in with (proper|incorrect) credentials$/) do |_situation|
+  fill_in 'username', with: 'test'
+
+  fill_in 'password', with: _situation == 'proper' ? 'test' : 'bad'
+  click_button 'Login'
 end
 
-Then(/^I should see the secret page$/) do
-  #put your code here
-  pending
+Then(/^I should see the (secret|error) page$/) do |_situation|
+  expect(page).to have_content if _situation == 'secret' ? 'This is the secret page' : 'Your username & password did not match'
 end
