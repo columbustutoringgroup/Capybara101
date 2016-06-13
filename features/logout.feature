27c5fe2d5c5133451dcb9a2d937050fd7724f264
Josh Meek <jmeek@covermymeds.com>
2016-06-13 13:30:28 -0400

    complete logout feature and add shared step helper

--- features/logout.feature
+++ features/logout.feature
@@ -1,1 +1,10 @@
-#Add your codez here
+Feature: Logout Page
+  As a user
+  After I have correctly logged in
+  I want to have to logout
+
+  Scenario: Logout
+    Given the login page
+    When I log in with proper credentials
+    When I logout
+    Then I should see the logout page
--- features/step_definitions/login_steps.rb
+++ features/step_definitions/login_steps.rb
@@ -8,7 +8,3 @@ When(/^I log in with (proper|incorrect) credentials$/) do |_situation|
   fill_in 'password', with: _situation == 'proper' ? 'test' : 'bad'
   click_button 'Login'
 end
-
-Then(/^I should see the (secret|error) page$/) do |_situation|
-  expect(page).to have_content if _situation == 'secret' ? 'This is the secret page' : 'Your username & password did not match'
-end
