830e2ca8469bbcc528e168e2bbe9dd58390655ab
Leah Musie <lmusie@covermymeds.com>
2017-01-18 11:50:17 -0500

    added steps, features, and all tests ran green

--- features/logout.feature
+++ features/logout.feature
@@ -1,1 +1,9 @@
-#Add your codez here
+Feature: Logout Page
+  In order to keep out the bad guys
+  As a user
+  I want to be able to logout when signed in
+
+  Scenario: Authenticated User
+    Given the logged in dashboard page
+    When I logout
+    Then I should see the logout page
--- features/step_definitions/logout_steps.rb
+++ features/step_definitions/logout_steps.rb
@@ -1,1 +1,16 @@
-#Add your codez here
+Given(/^the logged in dashboard page$/) do
+  visit('/')
+  within("#session") do
+    fill_in 'username', with: "leah"
+    fill_in 'password', with: "leah"
+  end
+  click_button 'Login'
+end
+
+When(/^I logout$/) do
+  click_link 'Logout'
+end
+
+Then(/^I should see the logout page$/) do
+  current_path.should== '/logout'
+end
