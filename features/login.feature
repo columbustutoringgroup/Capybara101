087a2f0617af575a758dfe931913b7a619f6d80e
Dan <dmeehan@covermymeds.com>
2017-05-22 10:39:29 -0400

    Added auth checks

--- features/login.feature
+++ features/login.feature
@@ -7,4 +7,15 @@ Feature: Login Page
     Given the login page
     When I log in with proper credentials
     Then I should see the secret page
-    
+
+  Scenario: Non-authenticated User
+    Given the login page
+    When I log in without proper credentials
+    Then I should see the login page
+      And I should get a notice that my credentials are invalid
+
+  Scenario: No User
+    Given the login page
+    When I log in with no credentials
+    Then I should see the login page
+      And I should get a notice that my credentials are invalid
--- features/step_definitions/login_steps.rb
+++ features/step_definitions/login_steps.rb
@@ -8,6 +8,24 @@ When(/^I log in with proper credentials$/) do
   click_button 'Login'
 end
 
+When(/^I log in without proper credentials$/) do
+  fill_in 'username', with: 'user'
+  fill_in 'password', with: 'pass'
+  click_button 'Login'
+end
+
+When(/^I log in with no credentials$/) do
+  click_button 'Login'
+end
+
 Then(/^I should see the secret page$/) do
   expect(page.current_path).to eq '/dashboard'
+end
+
+Then(/^I should see the login page$/) do
+  expect(page.current_path).to eq '/login'
+end
+
+Then(/^I should get a notice that my credentials are invalid$/) do
+  expect(page).to have_selector('small', text: 'Your username & password did not match')
 end
