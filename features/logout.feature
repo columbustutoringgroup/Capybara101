b80e0dec09d4663de2d26fe47f0170a65782e908
Matthew Crocco <matthewcrocco@gmail.com>
2017-06-06 00:40:46 -0400

    Submission commit

--- features/logout.feature
+++ features/logout.feature
@@ -1,1 +1,10 @@
-#Add your codez here
+Feature: Logout
+  In order to hide my details with others around
+  As a user
+  I want to be able to logout
+
+  Scenario: Authenticated User - Secret Page
+    Given I am authenticated
+    When I see the secret page
+    And I press logout
+    Then I should see the logout page
--- features/step_definitions/logout_steps.rb
+++ features/step_definitions/logout_steps.rb
@@ -1,1 +1,16 @@
-#Add your codez here
+Given(/I am authenticated/) do
+  visit('/')
+  step("I log in with the username 'Test' and the password 'Test'")
+end
+
+When(/I see the secret page/) do
+  visit('/dashboard')
+end
+
+When(/I press logout/) do
+  click_link('Logout')
+end
+
+Then(/I should see the logout page/) do
+  expect(page.current_path).to eq '/logout'
+end
