7361ad660b54d131d175e5c9ce4e067a2d02d5a6
David Hatten <dhatten@cmmc02vt0hzg8wl.innova.local>
2018-01-22 11:24:36 -0500

    added logout feature with steps

--- features/logout.feature
+++ features/logout.feature
@@ -1,1 +1,9 @@
-#Add your codez here
+Feature: Logout Page
+  In order to keep my account safe when I leave my desk
+  As a user
+  I want to be able to log out of my private page
+
+  Scenario: Logged-in User
+    Given A logged-in user
+    When I select the logout option
+    Then I should see the login page
--- features/step_definitions/logout_steps.rb
+++ features/step_definitions/logout_steps.rb
@@ -1,1 +1,15 @@
-#Add your codez here
+Given(/^A logged-in user$/) do
+  visit '/'
+  fill_in 'username', with: 'admin'
+  fill_in 'password', with: 'admin'
+  click_button 'Login'
+end
+
+When(/^I select the logout option$/) do
+  click_link 'Logout'
+end
+
+Then(/^I should see the login page$/) do
+  expect(page).to have_content 'You have been logged out'
+  expect(page).to have_content 'This is login page for users.'
+end
