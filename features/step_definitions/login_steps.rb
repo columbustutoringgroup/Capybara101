19b15f256d6981407443bedd960dfc657da994f5
David Hatten <dhatten@cmmc02vt0hzg8wl.innova.local>
2018-01-22 11:13:05 -0500

    added login_step definitions

--- features/step_definitions/login_steps.rb
+++ features/step_definitions/login_steps.rb
@@ -1,14 +1,13 @@
 Given(/^the login page$/) do
-  #put your code here
-  pending
+  visit '/'
 end
 
 When(/^I log in with proper credentials$/) do
-  #put your code here
-  pending
+  fill_in 'username', with: 'admin'
+  fill_in 'password', with: 'admin'
+  click_button 'Login'
 end
 
 Then(/^I should see the secret page$/) do
-  #put your code here
-  pending
+  expect(page).to have_content 'This is the secret page.'
 end
