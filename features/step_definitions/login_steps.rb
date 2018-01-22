ea27946ccb49f473d73cb8661fc4f8fa1850d576
Rupert Saxton <rsaxton@covermymeds.com>
2018-01-22 10:59:30 -0500

    Add login steps

--- features/step_definitions/login_steps.rb
+++ features/step_definitions/login_steps.rb
@@ -1,14 +1,14 @@
 Given(/^the login page$/) do
-  #put your code here
-  pending
+  visit 'https://localhost:4567'
 end
 
-When(/^I log in with proper credentials$/) do
-  #put your code here
-  pending
+When(/^I log in with username and password "(.*?)"$/) do |arg1|
+  fill_in 'username', with: arg1
+  fill_in 'password', with: arg1
+  click_button('Login')
 end
 
-Then(/^I should see the secret page$/) do
-  #put your code here
-  pending
+Then(/^I should see the secret page as "(.*?)"$/) do |arg1|
+  page.should have_content("This is the secret page.")
+  page.should have_content("Your username is #{arg1}.")
 end
