c229eda0c14afec2a0241fc0cf0ee7c018c1d391
Hari Dahal <HDahal@covermymeds.com>
2018-01-22 13:59:18 -0500

    completed logout feature

--- features/logout.feature
+++ features/logout.feature
@@ -1,1 +1,9 @@
-#Add your codez here
+Feature: Logout Page
+  Once I am done using the site
+  As a user
+  I want to logout
+
+Scenario: Authenticated User
+  Given a logged in page
+  When I logout
+  Then I should see logged out message
--- features/step_definitions/logout_steps.rb
+++ features/step_definitions/logout_steps.rb
@@ -1,1 +1,13 @@
-#Add your codez here
+Given(/^a logged in page$/) do
+  step "the login page"
+  step "I log in with proper credentials"
+  step "I should see the secret page"
+end
+
+When(/^I logout$/) do
+  click_link_or_button 'Logout'
+end
+
+Then(/^I should see logged out message$/) do
+  expect(page).to have_content 'You have been logged out'
+end
