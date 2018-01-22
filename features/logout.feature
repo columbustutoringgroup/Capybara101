8a21c15c88fb01d4c0c171ab14dc68b7abfb479e
Rupert Saxton <rsaxton@covermymeds.com>
2018-01-22 11:19:53 -0500

    Add logout features and steps

--- features/logout.feature
+++ features/logout.feature
@@ -1,1 +1,9 @@
-#Add your codez here
+Feature: Logout
+  In order to keep my account secure
+  As a user
+  I want to be able to log out
+
+  Scenario: Log out User
+    Given that "Maximus" is logged in
+    When they click the 'Logout' link
+    Then they should be logged out
--- features/step_definitions/logout_steps.rb
+++ features/step_definitions/logout_steps.rb
@@ -1,1 +1,17 @@
-#Add your codez here
+Given(/^that "(.*?)" is logged in$/) do |arg1|
+  visit 'https://localhost:4567'
+  fill_in 'username', with: arg1
+  fill_in 'password', with: arg1
+  click_button('Login')
+  page.should have_content("This is the secret page.")
+  page.should have_content("Your username is #{arg1}.")
+end
+
+When(/^they click the 'Logout' link$/) do
+  click_link('Logout')
+end
+
+Then(/^they should be logged out$/) do
+  expect(current_path).to eq '/logout'
+  page.should have_content('You have been logged out')
+end
