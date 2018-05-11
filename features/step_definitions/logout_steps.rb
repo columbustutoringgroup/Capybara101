ad4ee19e524638cc992c6859337a4f2f7554d297
Andrew Bonner <abonner@covermymeds.com>
2018-05-11 15:23:27 -0400

    Test logout feature.

--- features/step_definitions/logout_steps.rb
+++ features/step_definitions/logout_steps.rb
@@ -1,1 +1,18 @@
-#Add your codez here
+Given(/^that a user has logged in with the correct credentials$/) do
+  visit('/')
+  fill_in('username', with: 'user')
+  fill_in('password', with: 'user')
+  click_button('Login')
+end
+
+When(/^a user logs out$/) do
+  click_link('Logout')
+end
+
+Then(/^a logout message is displayed$/) do
+  expect(page).to have_content 'You have been logged out'
+end
+
+Then(/^the user is now on login page$/) do
+  expect(page).to have_content 'This is login page for users.'
+end
