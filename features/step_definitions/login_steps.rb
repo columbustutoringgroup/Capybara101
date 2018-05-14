25c549b1784852b9e75714652cfc007f3b68db49
Leslie Kornes <lkornes@covermymeds.com>
2018-05-14 14:12:24 -0400

    Write login_steps.

--- features/step_definitions/login_steps.rb
+++ features/step_definitions/login_steps.rb
@@ -1,14 +1,14 @@
 Given(/^the login page$/) do
-  #put your code here
-  pending
+  visit ('/')
 end
 
 When(/^I log in with proper credentials$/) do
-  #put your code here
-  pending
+  fill_in 'username', with: "JohnDoe"
+  fill_in 'password', with: "JohnDoe"
+  click_button 'Login'
 end
 
 Then(/^I should see the secret page$/) do
-  #put your code here
-  pending
+  expect(page).to have_content("This is the secret page.")
+  expect(page).to have_content("Your username is JohnDoe")
 end
