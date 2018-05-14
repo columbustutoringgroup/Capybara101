7c1bea751ddf0dbfaf29a54a00e86644ac8986f7
Leslie Kornes <lkornes@covermymeds.com>
2018-05-14 14:28:30 -0400

    Complete logout.feature and logout_steps.

--- features/step_definitions/logout_steps.rb
+++ features/step_definitions/logout_steps.rb
@@ -1,1 +1,15 @@
-#Add your codez here
+Given(/^the dashboard page$/) do
+  visit ('/')
+  fill_in 'username', with: "JohnDoe"
+  fill_in 'password', with: "JohnDoe"
+  click_button 'Login'
+  expect(page).to have_content("This is the secret page")
+end
+
+When(/^I click on the log out button$/) do
+  click_link 'Logout'
+end
+
+Then(/^I should see the log in page$/) do
+  expect(page).to have_link("Login")
+end
